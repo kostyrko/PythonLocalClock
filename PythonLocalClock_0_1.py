1da012077deb23116bf9c3e9abafb7f6ed7e36a3
kostyrko <48617990+kostyrko@users.noreply.github.com>
2019-04-22 08:38:24 +0200

    Create PythonLocalClock_0_1.py
    The initial file of a new project called PythonLocalClock

---
+++ PythonLocalClock_0_1.py
@@ -0,0 +1,25 @@
+'''application showing current time'''
+
+from tkinter import *
+import time
+
+win=Tk()
+win.title('Current Time')
+
+win.geometry('250x100')
+win.resizable(width=False,height=False)
+
+def get_time():
+   localtime = time.asctime( time.localtime(time.time()))
+   label2=Label(win, text=localtime)
+   label2.place(x=50,y=70)
+
+
+label1=Label(win, text='To display time push the button below')
+label1.place(x=20,y=0)
+
+btn1=Button(win, text='Show time' , command= lambda: get_time())
+btn1.place(x=90,y=30)
+
+
+win.mainloop()
